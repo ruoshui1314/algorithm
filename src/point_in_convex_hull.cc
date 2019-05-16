4dd1e8c3e5146113e84dc1c9fbfb7fbf2b836fdc
weiy <wei.yuan@uisee.com>
2019-05-16 09:34:55 +0800

    add check point in convex hull

--- src/point_in_convex_hull.cc
+++ src/point_in_convex_hull.cc
@@ -40,7 +40,14 @@ public:
 float cross(const Point& p0, const Point& p1, const Point& p2) {
     return (p1.x - p0.x) * (p2.y - p0.y) - (p2.x - p0.x) * (p1.y - p0.y);
 }
- 
+
+bool onsegment(const Point& pi, const Point& pj, const Point& pk) {
+    if (min(pi.x, pj.x) <= pk.x && max(pi.x, pj.x) >= pk.x && min(pi.y, pj.y) <= pk.y && max(pi.y, pj.y) >= pk.y)
+        return true;
+    return false;
+}
+
+// check point in convex hull
 bool point_in_convex_hull(const std::vector<Point>& points, const Point& p) {
     size_t length = points.size();
     if(length < 3) return false;
@@ -51,14 +58,19 @@ bool point_in_convex_hull(const std::vector<Point>& points, const Point& p) {
 
     while(i <= j) {
         mid = (i + j) >> 1;
-        double a1 = cross(points[0], points[mid], p);
-        double a2 = cross(points[0], points[mid + 1], p);
-        if(a1 > FLOAT_EPS && a2 < FLOAT_EPS) {
-            if(cross(points[mid], points[mid+1], p) > FLOAT_EPS)
+        float a1 = cross(points[0], points[mid], p);
+        float a2 = cross(points[0], points[mid + 1], p);
+        if(a1 > FLOAT_EPS && a2 < -FLOAT_EPS) {
+            float a = cross(points[mid], points[mid+1], p);
+            if(a > FLOAT_EPS || (abs(a) < FLOAT_EPS && onsegment(points[mid], points[mid + 1], p)))
                 return true;
             return false;
         }
-        else if (a1 < FLOAT_EPS)
+        else if (abs(a1) < FLOAT_EPS && mid == 1 && onsegment(points[0], points[mid], p))
+            return true;
+        else if (abs(a2) < FLOAT_EPS && mid + 1 == length - 1 && onsegment(points[0], points[mid + 1], p))
+            return true;
+        else if (a1 < -FLOAT_EPS)
             j = mid -1;
         else
             i = mid + 1;
@@ -69,12 +81,14 @@ bool point_in_convex_hull(const std::vector<Point>& points, const Point& p) {
 int main() {
     vector<Point> points;
     points.push_back(Point(0, 0));
-    points.push_back(Point(3, 1));
+    points.push_back(Point(4, 0));
     points.push_back(Point(4, 4));
     Point p(2, 2);
-    cout << point_in_convex_hull(points, p) << endl;
+    cout << point_in_convex_hull(points, p) << endl; // true
     p = Point(-1, -1);
-    cout << point_in_convex_hull(points, p) << endl;
+    cout << point_in_convex_hull(points, p) << endl; // false
+    p = Point(4, 2);
+    cout << point_in_convex_hull(points, p) << endl; // true
     p = Point(0, 0);
-    cout << point_in_convex_hull(points, p) << endl;
+    cout << point_in_convex_hull(points, p) << endl; // true
 }
